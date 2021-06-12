3d5cbf482af469925e3351e74f264bd8a21bf951
kompasim <kompasim@163.com>
2021-06-13 00:40:47 +0800

    update

--- source/render.c
+++ source/render.c
@@ -20,14 +20,10 @@ void doRender()
     SDL_RenderPresent(renderer);
 }
 
-void drawImage(char *path, int x, int y, int w, int h, int toX, int toY, int toW, int toH, bool isFlipX, bool isFlipY, int angle, float anchorX, float anchorY)
+SDL_Point parseTexture(SDL_Texture *texture, int x, int y, int w, int h, int toX, int toY, int toW, int toH, float anchorX, float anchorY, SDL_Rect *src, SDL_Rect *dst)
 {
-    SDL_Surface *imageSurface = IMG_Load(path);
-    SDL_Texture *imageTexture = SDL_CreateTextureFromSurface(renderer, imageSurface);
     int textureW, textureH;
-    int windowW, windowH;
-    SDL_QueryTexture(imageTexture, NULL, NULL, &textureW, &textureH);
-    SDL_GetWindowSize(window, &windowW, &windowH);
+    SDL_QueryTexture(texture, NULL, NULL, &textureW, &textureH);
     x = get_min(get_max(x, 0), textureW);
     y = get_min(get_max(y, 0), textureH);
     w = get_min(get_max(w, 0), textureW);
@@ -36,55 +32,55 @@ void drawImage(char *path, int x, int y, int w, int h, int toX, int toY, int toW
     int leftH = (textureH - y);
     w = w > 0 ? w : leftW;
     h = h > 0 ? h : leftH;
-    toX = get_min(get_max(toX, 0), windowW);
-    toY = get_min(get_max(toY, 0), windowH);
-    toW = get_min(get_max(toW, 0), windowW);
-    toH = get_min(get_max(toH, 0), windowH);
     toW = toW > 0 ? toW : leftW;
     toH = toH > 0 ? toH : leftH;
     int indentX = toW * anchorX;
     int indentY = toH * anchorY;
-    SDL_Rect srcRect = {x, y, w, h};
-    SDL_Rect sdtRect = {toX - indentX, toY - indentY, toW, toH};
+    (*src).x = x;
+    (*src).y = y;
+    (*src).w = w;
+    (*src).h = h;
+    (*dst).x = toX - indentX;
+    (*dst).y = toY - indentY;
+    (*dst).w = toW;
+    (*dst).h = toH;
+    SDL_Point point = {toW * anchorX, toW * anchorY};
+    return point;
+}
+
+void drawImage(char *path, int x, int y, int w, int h, int toX, int toY, int toW, int toH, bool isFlipX, bool isFlipY, int angle, float anchorX, float anchorY)
+{
+    SDL_Texture *texture = IMG_LoadTexture(renderer, path);
+    SDL_Rect srcRect, dstRect;
+    SDL_Point point = parseTexture(texture, x, y, w, h, toX, toY, toW, toH, anchorX, anchorY, &srcRect, &dstRect);
     SDL_RendererFlip flip = SDL_FLIP_NONE;
     if (isFlipX) flip |= SDL_FLIP_HORIZONTAL;
     if (isFlipY) flip |= SDL_FLIP_VERTICAL;
-    SDL_Point point = {toW * anchorX, toW * anchorY};
-    SDL_RenderCopyEx(renderer, imageTexture, &srcRect, &sdtRect, angle, &point, flip);
-    SDL_DestroyTexture(imageTexture);
-    SDL_FreeSurface(imageSurface);
+    SDL_RenderCopyEx(renderer, texture, &srcRect, &dstRect, angle, &point, flip);
+    SDL_DestroyTexture(texture);
     if (automatic) doRender();
 }
 
-void drawText(char *text, int toX, int toY, char *font, int size, bool isFlipX, bool isFlipY, int angle, float anchorX, float anchorY)
+void drawText(char *text, int x, int y, int w, int h, int toX, int toY, int toW, int toH, char *font, int size, bool isFlipX, bool isFlipY, int angle, float anchorX, float anchorY)
 {
-    font = strlen(font) > 0 ? font : "alkatip.ttf";
+    font = strlen(font) > 0 ? font : "ukij.ttf";
     size = get_min(get_max(size, 1), 100);
     TTF_Font *ttf = TTF_OpenFont(font, size);
     assert(ttf != NULL, "ttf open failed!");
     Uint8 r, g, b, a;
     SDL_GetRenderDrawColor(renderer, &r, &g, &b, &a);
     SDL_Color color = {r, g, b, a};
-    SDL_Surface *textSurface = TTF_RenderUTF8_Blended(ttf, text, color);
-    SDL_Texture *textTexture = SDL_CreateTextureFromSurface(renderer, textSurface);
-    int textureW, textureH;
-    int windowW, windowH;
-    SDL_QueryTexture(textTexture, NULL, NULL, &textureW, &textureH);
-    SDL_GetWindowSize(window, &windowW, &windowH);
-    toX = get_min(get_max(toX, 0), windowW);
-    toY = get_min(get_max(toY, 0), windowH);
-    int indentX = textureW * anchorX;
-    int indentY = textureH * anchorY;
-    SDL_Rect srcRect = {0, 0, textureW, textureH};
-    SDL_Rect sdtRect = {toX - indentX, toY - indentY, textureW, textureH};
+    SDL_Surface *surface = TTF_RenderUTF8_Blended(ttf, text, color);
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_Rect srcRect, dstRect;
+    SDL_Point point = parseTexture(texture, x, y, w, h, toX, toY, toW, toH, anchorX, anchorY, &srcRect, &dstRect);
     SDL_RendererFlip flip = SDL_FLIP_NONE;
     if (isFlipX) flip |= SDL_FLIP_HORIZONTAL;
     if (isFlipY) flip |= SDL_FLIP_VERTICAL;
-    SDL_Point point = {textureW * anchorX, textureH * anchorY};
-    SDL_RenderCopyEx(renderer, textTexture, &srcRect, &sdtRect, angle, &point, flip);
+    SDL_RenderCopyEx(renderer, texture, &srcRect, &dstRect, angle, &point, flip);
     TTF_CloseFont(ttf);
-    SDL_FreeSurface(textSurface);
-    SDL_DestroyTexture(textTexture);
+    SDL_FreeSurface(surface);
+    SDL_DestroyTexture(texture);
     if (automatic) doRender();
 }
 
@@ -179,9 +175,9 @@ void renderTest()
     char *text2 = "!ﺎﻴﻧﯗﺩ ﺎﺑﺎﮬﺭﻪﻣ";
     char *font = "ukij.ttf";
     setColor(200, 0, 0, 255);
-    drawText(text1, 100, 200, font, 24, true, false, 0, 0.5, 0.5);
-    drawText(text1, 100, 225, font, 24, false, false, 0, 0.5, 0.5);
-    drawText(text2, 100, 250, font, 24, false, false, 0, 0.5, 0.5);
+    drawText(text1, 0, 0, 0, 0, 100, 200, 0, 0, font, 24, true, false, 0, 0.5, 0.5);
+    drawText(text1, 50, 0, 0, 0, 100, 225, 0, 0, font, 24, false, false, 0, 0.5, 0.5);
+    drawText(text2, 0, 0, 0, 0, 100, 250, 0, 0, font, 24, false, false, 0, 0.5, 0.5);
     //
     setColor(0, 200, 0, 255);
     fillRect(150, 50, 50, 50);
